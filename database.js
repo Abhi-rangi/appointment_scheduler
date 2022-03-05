75930a06b1d25887bb6ddf5035c929a06aa66647
Abhi-rangi <abhishekrangi9@gmail.com>
2022-03-05 13:34:43 +0530

    getFunction

--- database.js
+++ database.js
@@ -1,26 +1,66 @@
-
+import sqlite3 from "sqlite3";
 const DBSOURCE = "db.sqlite";
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
       // Cannot open database
       console.error(err.message)
       throw err
-    }else{
+    }
+    else{
         console.log('Connected to the SQLite database.');
-        db.run(`CREATE TABLE appointment (
-            appoitment_number interger, appointment_time varchar(30) , hni_id integer, wm_id integer
-            `);
+            try{ 
+                // db.run('CREATE TABLE appointment ( appno INTEGER PRIMARY KEY AUTOINCREMENT,date vartchar(30),time varchar(30) ,hni_id varchar(30),wm_id varchar(30),flag boolean,status varchar(30))');
+                // db.run('CREATE TABLE HNI (hni_id varchar(30) PRIMARY KEY,status varchar(30),appno integer,date varchar(30),time varchar(30))');
+                // db.run('CREATE TABLE WM (wm_id varchar(30) PRIMARY KEY,status varchar(30),appno integer,date varchar(30),time varchar(30))');
+                console.log("done!!!!")
+                }
+                catch(e){
+                    console.error(e);
+                }
         }
-}
+    }
 );
 export async function bookAppointment(body){
-    // const flag = body.flag;
-    if(flag == 0){
-        var insert = 'INSERT INTO appointment (appoitment_number, appointment_time, hni_id,wm_id) VALUES (1,"10:30",2,3)';
-        db.run(insert);
+    const flag = body.flag;
+    const appno = body.appno;
+    const hni_id = body.hni_id;
+    const wm_id = body.wm_id;
+    const date = JSON.stringify(body.date);
+    const time = JSON.stringify(body.time); 
+
+    const status = "not confirmed";
+    db.run('INSERT INTO appointment (appno,date, time, hni_id,wm_id,flag,status)' + 'values (?,?,?,?,?,?,?)', [appno, date, time,hni_id,wm_id,flag,status]);
+    if(flag){
+      
+        db.run('INSERT INTO HNI (hni_id,status,appno,date ,time)' + 'values (?,?,?,?,?)', [hni_id,"accept pending",appno,date,time]);
+        db.run('INSERT INTO WM (wm_id,status,appno,date ,time)' + 'values (?,?,?,?,?)', [wm_id,status,appno,date,time]);
     }
+    else{
+        db.run('INSERT INTO HNI (hni_id,status,appno,date ,time)' + 'values (?,?,?,?,?)', [hni_id,status,appno,date,time]);
+        db.run('INSERT INTO WM (wm_id,status,appno,date ,time)' + 'values (?,?,?,?,?)', [wm_id,"accept pending",appno,date,time]);
+    }
+    console.log("Appointment booked succefully");
     return {"message":"Appointment booked succefully"};
 }
-// export async function accpet_HNI(){
+export async function Accept(body){
+    db.run("UPDATE HNI set status = ? where appno = ?", ["accept",body.appno ]);
+    db.run("UPDATE WM set status = ? where appno = ?",["accept",body.appno]);
+    db.run("UPDATE appointment set status = ? where appno = ?",["accept",body.appno]);
+    return{"message":"accepted"};
+}
+export async function Reject(body){
+    db.run("UPDATE HNI set status = ? where appno = ?", ["reject",body.appno ]);
+    db.run("UPDATE WM set status = ? where appno = ?",["reject",body.appno]);
+    db.run("UPDATE appointment set status = ? where appno = ?",["reject",body.appno]);
+    return{"message":"rejected"};
+}export async function Reschedule(body){
 
-// }
+    return 0 ;
+}
+export async function GetHNI(){{
+    let result = db.get('select * from HNI where appno=?',2);
+    console.log(result);
+    return JSON.stringify(result);
+}
+
+} 
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // import sqlite3 from "sqlite3";
 import express from "express";
-import { bookAppointment } from "./database";
+import { bookAppointment ,Accept,Reject,Reschedule,GetHNI} from "./database.js";
 var app = express();
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json({ limit: "10mb" }));
+let appno = 1;
 const HTTP_PORT = 8000
 app.listen(HTTP_PORT, () => {
     console.log("Server is listening on port " + HTTP_PORT);
@@ -12,9 +13,31 @@ app.listen(HTTP_PORT, () => {
 app.get("/", (req,res) => {
     res.json({"message":"Ok"})
 });
-app.post("/bookAppointment",(req,res)=>{
-    res.send(await bookAppointment(req.body));
+app.post("/bookAppointment",async(req,res)=>{
+    req.body.appno = appno;
+    appno++;
+    let book = await bookAppointment(req.body) ;
+    res.send(book);
 });
+app.post("/accept",async(req,res)=>{
+    let accept = await Accept(req.body) ;
+    res.send(accept);
+});
+app.post("/reject",async(req,res)=>{
+    let reject = await Reject(req.body) ;
+    res.send(reject);
+});
+app.post("/reschedule",async(req,res)=>{
+    let reschedule = await Reschedule(req.body) ;
+    res.send(reschedule);
+});
+app.get('/getHNI',async(req,res)=>{
+    let getHNI = await GetHNI() ;
+    res.send(getHNI);
+});
+
+
+
 // app.get("/HNI/accept",(req,res)=>{
 //     const result = book
 //     res.send(response);
